6aecb5cfb88c0d5a991b328e4ae36c8ff7f3b4e4
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-05 23:16:40 -0300

    Export profile APIs

--- engine/build/__init__.py
+++ engine/build/__init__.py
@@ -1,1 +1,14 @@
 from __future__ import annotations
+
+from .build_config import BuildConfig, BuildTarget
+from .export_profile import ExportProfile, debug_profile, release_profile
+from .export_profile_manager import ExportProfileManager
+
+__all__ = [
+    "BuildConfig",
+    "BuildTarget",
+    "ExportProfile",
+    "ExportProfileManager",
+    "debug_profile",
+    "release_profile",
+]
