5841a0a7ff103fa0c4500cd23655f7e9cf7dab25
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-05 23:04:00 -0300

    Add build pipeline package

---
+++ engine/build/__init__.py
@@ -0,0 +1,12 @@
+from __future__ import annotations
+
+from .build_config import BuildConfig, BuildTarget
+from .build_pipeline import BuildPipeline, BuildReport, BuildStepResult
+
+__all__ = [
+    "BuildConfig",
+    "BuildTarget",
+    "BuildPipeline",
+    "BuildReport",
+    "BuildStepResult",
+]
