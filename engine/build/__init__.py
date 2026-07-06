8d8e71857ef067237500aed07a502a27b94bc8c4
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-05 23:23:27 -0300

    Export desktop package APIs

--- engine/build/__init__.py
+++ engine/build/__init__.py
@@ -1,14 +1,18 @@
 from __future__ import annotations
 
 from .build_config import BuildConfig, BuildTarget
+from .desktop_package import DesktopPackagePlan, create_desktop_package_plan, create_plan_from_profile
 from .export_profile import ExportProfile, debug_profile, release_profile
 from .export_profile_manager import ExportProfileManager
 
 __all__ = [
     "BuildConfig",
     "BuildTarget",
+    "DesktopPackagePlan",
     "ExportProfile",
     "ExportProfileManager",
+    "create_desktop_package_plan",
+    "create_plan_from_profile",
     "debug_profile",
     "release_profile",
 ]
