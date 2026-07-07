05a670e6b80e76dfa50120ddc576b73298d7b642
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-06 22:41:11 -0300

    Register enhanced inspector plugins

--- editor/inspector/__init__.py
+++ editor/inspector/__init__.py
@@ -1,10 +1,12 @@
 from editor.inspector.plugin import InspectorPlugin
 from editor.inspector.plugin_registry import InspectorPluginRegistry, inspector_plugin_registry
 from editor.inspector.default_plugins import register_default_inspector_plugins
+from editor.inspector.asset_component_plugins import register_asset_component_plugins
 
 __all__ = [
     "InspectorPlugin",
     "InspectorPluginRegistry",
     "inspector_plugin_registry",
     "register_default_inspector_plugins",
+    "register_asset_component_plugins",
 ]
