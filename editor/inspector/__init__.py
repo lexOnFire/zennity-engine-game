96a486c5cb2eabacf9d0117a783497a3033f3f14
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-07 18:27:34 -0300

    Register direct asset drop runtime patch

--- editor/inspector/__init__.py
+++ editor/inspector/__init__.py
@@ -2,6 +2,9 @@ from editor.inspector.plugin import InspectorPlugin
 from editor.inspector.plugin_registry import InspectorPluginRegistry, inspector_plugin_registry
 from editor.inspector.default_plugins import register_default_inspector_plugins
 from editor.inspector.asset_component_plugins import register_asset_component_plugins
+from editor.runtime.asset_direct_drop_patch import patch_asset_direct_drop_runtime
+
+patch_asset_direct_drop_runtime()
 
 __all__ = [
     "InspectorPlugin",
