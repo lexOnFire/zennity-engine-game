1a3326de8b7d847af601fd20b3fcc9d2770467df
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-07 18:01:09 -0300

    Add X and Z labels to move gizmo

--- editor/gizmos/qt_gizmo_overlay.py
+++ editor/gizmos/qt_gizmo_overlay.py
@@ -27,13 +27,13 @@ class QtMoveGizmoOverlay:
         cx, cy = world_to_viewport(pos)
         center = QPointF(cx, cy)
         x_end = QPointF(cx + self.axis_length, cy)
-        y_end = QPointF(cx, cy - self.axis_length)
+        z_end = QPointF(cx, cy - self.axis_length)
 
         painter.save()
         painter.setRenderHint(QPainter.Antialiasing, True)
 
         self._draw_axis(painter, center, x_end, QColor(230, 70, 70), "x")
-        self._draw_axis(painter, center, y_end, QColor(70, 210, 90), "y")
+        self._draw_axis(painter, center, z_end, QColor(70, 210, 90), "z")
 
         painter.setPen(QPen(QColor(15, 15, 15), 1))
         painter.setBrush(QBrush(QColor(245, 245, 245)))
@@ -51,10 +51,16 @@ class QtMoveGizmoOverlay:
                 QPointF(end.x(), end.y() - 7),
                 QPointF(end.x(), end.y() + 7),
             ])
+            label_pos = QPointF(end.x() + 18, end.y() + 5)
+            label = "X"
         else:
             head = QPolygonF([
                 QPointF(end.x(), end.y() - 12),
                 QPointF(end.x() - 7, end.y()),
                 QPointF(end.x() + 7, end.y()),
             ])
+            label_pos = QPointF(end.x() + 8, end.y() - 14)
+            label = "Z"
         painter.drawPolygon(head)
+        painter.setPen(QPen(color, 1, Qt.SolidLine))
+        painter.drawText(label_pos, label)
