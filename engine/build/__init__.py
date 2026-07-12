8c017b1769ed5314d61ee814594398cd3526f659
Alex <121579863+lexOnFire@users.noreply.github.com>
2026-07-12 18:05:43 -0300

    feat(editor): add console filters prefabs and project export

--- engine/build/__init__.py
+++ engine/build/__init__.py
@@ -4,6 +4,7 @@ from .build_config import BuildConfig, BuildTarget
 from .desktop_package import DesktopPackagePlan, create_desktop_package_plan, create_plan_from_profile
 from .export_profile import ExportProfile, debug_profile, release_profile
 from .export_profile_manager import ExportProfileManager
+from .project_exporter import export_development_project
 
 __all__ = [
     "BuildConfig",
@@ -15,4 +16,5 @@ __all__ = [
     "create_plan_from_profile",
     "debug_profile",
     "release_profile",
+    "export_development_project",
 ]
