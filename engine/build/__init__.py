dbe051f86c272901cbf68e835d2112e748665ea4
lexOnFire <lexgodfire@gmail.com>
2026-07-25 13:30:04 -0300

    fix(build): re-export ProjectValidationReport, ValidationIssue, validate_project, and BuildPipeline stages in engine.build package

--- engine/build/__init__.py
+++ engine/build/__init__.py
@@ -1,7 +1,25 @@
 from engine.build.pipeline import BuildProfile, BuildPipeline, BuildReport
+from engine.build.project_validator import ProjectValidationReport, ValidationIssue, validate_project
+from engine.build.stages import (
+    ResolveDependenciesStage,
+    AssetCookingStage,
+    AssetCompressionStage,
+    PackageGenerationStage,
+    ManifestGenerationStage,
+    ExecutableLinkStage,
+)
 
 __all__ = [
     "BuildProfile",
     "BuildPipeline",
     "BuildReport",
+    "ProjectValidationReport",
+    "ValidationIssue",
+    "validate_project",
+    "ResolveDependenciesStage",
+    "AssetCookingStage",
+    "AssetCompressionStage",
+    "PackageGenerationStage",
+    "ManifestGenerationStage",
+    "ExecutableLinkStage",
 ]
