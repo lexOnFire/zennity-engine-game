fbc4142cbb285797f20a686a9677f90ea0d3fdcf
lexOnFire <lexgodfire@gmail.com>
2026-07-25 13:26:28 -0300

    fix(runtime): re-export RuntimeManager and RuntimeState in engine.runtime package

--- engine/runtime/__init__.py
+++ engine/runtime/__init__.py
@@ -1,13 +1,18 @@
+from engine.runtime.runtime_manager import RuntimeManager, RuntimeState
 from engine.runtime.provider import RuntimeProvider
 from engine.runtime.production_runtime import (
     SceneStreamingService,
     ResourceManagerCache,
     JobScheduler,
+    JobTask,
 )
 
 __all__ = [
+    "RuntimeManager",
+    "RuntimeState",
     "RuntimeProvider",
     "SceneStreamingService",
     "ResourceManagerCache",
     "JobScheduler",
+    "JobTask",
 ]
