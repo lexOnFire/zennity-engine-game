1cc9cdaed34f66bb5f81061adf837315c26f71a1
lexOnFire <lexgodfire@gmail.com>
2026-07-30 21:59:23 -0300

    chore: Pre-Release Cleanup - Remove deprecated legacy entrypoints

--- editor/editor_app/application.py
+++ editor/editor_app/application.py
@@ -3,13 +3,9 @@ from __future__ import annotations
 
 import sys
 
-from editor.editor_app.bootstrap import run_isolated_editor, run_legacy_embedded_editor
+from editor.editor_app.bootstrap import run_isolated_editor
 
 
 def main(argv: list[str] | None = None) -> None:
     """Start the single official editor shell."""
-    args = list(sys.argv if argv is None else argv)
-    if "--legacy-embedded" in args:
-        run_legacy_embedded_editor(args)
-        return
     run_isolated_editor()
